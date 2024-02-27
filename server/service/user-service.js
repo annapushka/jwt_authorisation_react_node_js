819e8f93bf5af949d56cc45526ad378b28c97c10
anna <anna.pushkareva@smddev.com>
2024-02-27 15:27:45 +0300

    add user controller, models, services, api-error middleware

---
+++ server/service/user-service.js
@@ -0,0 +1,67 @@
+const UserModel = require('../models/user-model')
+const bcrypt = require('bcrypt')
+const uuid = require('uuid')
+const mailService = require('./mail-service')
+const tokenService = require('./token-service')
+const UserDto = require('../dtos/user-dto')
+const ApiError = require('../exeptions/api-error')
+
+class UserService {
+    async registration(email, password) {
+        const candidate = await UserModel.findOne({email})
+        if (candidate) {
+            throw ApiError.BadRequest(`User with this email ${email} already exists`)
+        }
+
+        const hashPassword = await bcrypt.hash(password, 3)
+        const activationLink = uuid.v4()
+
+        const user = await UserModel.create({email, password: hashPassword, activationLink})
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`)
+
+        const userDto = new UserDto(user)
+        const tokens = tokenService.generateTokens({...userDto})
+        await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+        return {...tokens, user: userDto}
+    }
+
+    async activate(activationLink) {
+        const user = await UserModel.findOne({activationLink})
+        if (!user) {
+            throw ApiError.BadRequest('Incorrect activation link')
+        }
+        user.isActivated = true
+        await user.save()
+    }
+
+    async login(email, password) {
+        const user = await UserModel.findOne({email})
+        if (!user) {
+            throw ApiError.BadRequest('User with this email not found')
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password)
+        if (!isPassEquals) {
+            throw ApiError.BadRequest('Incorrect password')
+        }
+        const userDto = new UserDto(user)
+        const tokens = tokenService.generateTokens({...userDto})
+
+        await tokenService.saveToken(userDto.id, tokens.refreshToken)
+        return {...tokens, user: userDto}
+    }
+
+    async logout(refreshToken) {
+        return await tokenService.removeToken(refreshToken)
+    }
+
+    async refresh(refreshToken) {
+    }
+
+    async getAllUsers() {
+        const users = await UserModel.find()
+        return users
+    }
+}
+
+module.exports = new UserService()
